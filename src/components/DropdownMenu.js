20785bee23eb434cdef4795602d0a5b28453399f
Rasha Moumneh <rasha@fastmail.com>
2020-08-31 19:21:14 -0400

    Main menu

---
+++ src/components/DropdownMenu.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ReactComponent as CogIcon } from "../icons/cog.svg";
+import { ReactComponent as ChevronIcon } from "../icons/chevron.svg";
+import DropdownItem from "./DropdownItem";
+
+const DropdownMenu = () => {
+  return (
+    <div className="dropdown">
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem leftIcon={<CogIcon />} rightIcon={<ChevronIcon />}>
+        Settings
+      </DropdownItem>
+    </div>
+  );
+};
+
+export default DropdownMenu;
